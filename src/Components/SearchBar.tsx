a69b7007a1562f20ca4dfc2ed229c3b183a6592c
Kelvin <kelvinmoe96@gmail.com>
2025-10-28 23:57:17 +0800

    adding some inputs

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -7,6 +7,16 @@ const SearchBar = () => {
           placeholder="Search products.."
           className="border-2 rounded-md "
         />
+        <input
+          type="text"
+          placeholder="Products Categories"
+          className="border-2 rounded-md"
+        />
+        <select>
+          <option>this one</option>
+          <option>that one </option>
+          <option>those ones</option>
+        </select>
       </form>
     </>
   );

6bca72495cf936bcb212a084156dcf921c2477a9
Kelvin <kelvinmoe96@gmail.com>
2025-10-31 23:59:35 +0800

    foooter

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="max-w-full bg-purple-500">
+        <div className="grid-cols-4">
+          <div>ShopHub</div>
+          <div>ShopHub</div>
+          <div>ShopHub</div>
+          <div>ShopHub</div>
+        </div>
+      </footer>
+      Footer
+    </>
+  );
+};
+
+export default Footer;
